0aec866b43bf28b9f8a2ba68097be007dcc3ddb8
mrkn <markiian.design@gmail.com>
2024-08-01 12:56:01 +0200

    Upd

--- Donatan/settings.py
+++ Donatan/settings.py
@@ -139,3 +139,11 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vatikan98@gmail.com'
+EMAIL_HOST_PASSWORD = 'byro zcxk hqmb vlyc'
+EMAIL_USE_TLS = True
+
