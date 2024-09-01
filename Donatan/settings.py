bbce61b9926654afc8f07c882b387d3496f0380e
Andriy Kushneruk <akushneruk@rebbix.com>
2024-09-01 14:13:34 +0300

    turn off debug

--- Donatan/settings.py
+++ Donatan/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.donatan.com.ua', 'donatan.com.ua', 'localhost','127.0.0.1']
 
